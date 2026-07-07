774bdcb5dd49cd1c734d77d1507e9c032e9a4757
agnesgalvaowellhub <agnes.galvao@gympass.com>
2026-07-07 12:58:01 -0300

    feat: adding application class

---
+++ src/main/java/com/inter/remittance/RemittanceServiceApplication.java
@@ -0,0 +1,17 @@
+package com.inter.remittance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableCaching
+@EnableScheduling
+public class RemittanceServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RemittanceServiceApplication.class, args);
+	}
+}
